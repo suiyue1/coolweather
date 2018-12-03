c3adcc76d24f176da287f05734641c908ba01f82
John Doe <johndoe@example.com>
2018-12-03 22:31:44 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,97 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.County;
+import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utility {
+
+    /*
+     * 解析和处理服务器返回的省级数据
+     * */
+    public static boolean sendleProvinceResponse(String response){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i= 0;i<allProvinces.length();i++){
+                    JSONObject provincesObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provincesObject.getString("name"));
+                    province.setProvinceCode(provincesObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            }catch (JSONException e ) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /*
+    * 解析和处理服务器返回的市级数据
+    * */
+    public static boolean sendleCityResponse(String response, int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i<allCities.length() ;i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }return false;
+    }
+
+    public static boolean sendleCountyResponse(String response, int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0 ; i<allCounties.length() ;i++){
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+    /*
+    * 将返回的JSON数据解析成Weather的实体类
+    * */
+
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return  new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
